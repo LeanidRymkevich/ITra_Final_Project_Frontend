7181dab7e1f4bf1d2df548912bd94462a609955f
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-09-30 23:12:30 +0300

    feat: add sample project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App: FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 style={{ textAlign: "center" }}>Root Page</h1>}
+        />
+        <Route
+          path="/admins"
+          element={<h1 style={{ textAlign: "center" }}>Admins Page</h1>}
+        />
+        <Route
+          path="/login"
+          element={<h1 style={{ textAlign: "center" }}>Login Page</h1>}
+        />
+        <Route
+          path="*"
+          element={<h1 style={{ textAlign: "center" }}>Not Found</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
