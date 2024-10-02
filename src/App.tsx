085c98b58a9ff4a080ed6ef4d3c42cef3ccc529c
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-10-02 19:57:54 +0300

    feat: add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FC } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: FC = () => {
   return (
@@ -7,19 +7,19 @@ const App: FC = () => {
       <Routes>
         <Route
           path="/"
-          element={<h1 style={{ textAlign: "center" }}>Root Test Page</h1>}
+          element={<h1 style={{ textAlign: 'center' }}>Root Test Page</h1>}
         />
         <Route
           path="/admins"
-          element={<h1 style={{ textAlign: "center" }}>Admins Page</h1>}
+          element={<h1 style={{ textAlign: 'center' }}>Admins Page</h1>}
         />
         <Route
           path="/login"
-          element={<h1 style={{ textAlign: "center" }}>Login Page</h1>}
+          element={<h1 style={{ textAlign: 'center' }}>Login Page</h1>}
         />
         <Route
           path="*"
-          element={<h1 style={{ textAlign: "center" }}>Not Found</h1>}
+          element={<h1 style={{ textAlign: 'center' }}>Not Found</h1>}
         />
       </Routes>
     </BrowserRouter>
